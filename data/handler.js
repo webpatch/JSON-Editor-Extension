cff01d5661ddf4c31d65efb1b452a05132f87842
Clinic <clinic@ClinicdeMacBook-Pro.local>
2015-05-04 14:54:28 +0800

    another version

--- data/handler.js
+++ data/handler.js
@@ -1,9 +1,54 @@
-app.load()
-app.resize()
-app.setData(self.options.jsonText)
-codeEditor.format()
-document.getElementsByClassName("poweredBy")[0].onclick = function(e){
-	e.preventDefault();
-	app.notify.showNotification("Text copied to clipboard!",1500);
-	self.port.emit("copy",codeEditor.getText())
-}
+(function(){
+	app.load();
+	app.resize();
+	reloadEditor(self.options.jsonText)
+	
+	function reloadEditor(data){
+		app.setData(data);
+		codeEditor.format();
+	}
+
+	function getE(id)
+	{
+		const i = id.charAt(0);
+		id = id.substr(1);
+		if(i =='#'){
+			return document.getElementById(id);
+		}else if(i == '.'){
+			return document.getElementsByClassName(id)[0];
+		}
+		return null;
+	}
+
+	getE(".poweredBy").onclick = function(e){
+		e.preventDefault();
+		app.notify.showNotification("Text copied to clipboard!",1500);
+		self.port.emit("copy",codeEditor.getText());
+	};
+	
+	var btn = getE("#reloadBtn");
+	btn.onclick = function(){
+		self.port.emit("reload",input.value);
+	};
+
+	var input = getE("#inputURL");
+	input.value = self.options.url;
+	input.onkeypress = function(e){
+		if(e.keyCode == 13){
+			self.port.emit("reload",input.value);
+			return false;
+		}
+	}
+
+	self.port.on('reloadSuccess',function(txt){
+		reloadEditor(txt);
+		input.blur();
+		input.focus();
+	});
+
+	self.port.on('reloadFail',function(txt){
+		txt =  txt || "";
+		app.notify.showError("Reload failed!\n"+txt);
+	});
+
+})(window);
